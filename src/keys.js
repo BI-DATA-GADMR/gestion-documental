bddbb9665e79aaeea74ed42c2aa03ff69aa6267f
sebas <78992252+SebasMena28@users.noreply.github.com>
2023-10-20 17:25:49 -0500

    sfd ads
    sdf

---
+++ src/keys.js
@@ -0,0 +1,15 @@
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'ocu-sis';
+const DB_PORT = 3306;
+
+module.exports = {
+    database: {
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        port: DB_PORT
+    }
+}
